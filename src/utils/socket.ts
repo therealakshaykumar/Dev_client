15e29a4ab4602c433e8d2f87fe529484c8e661c3
Akshay <akshay.kumar@eazyerp.com>
2026-02-16 22:25:26 +0530

    Test

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -4,13 +4,10 @@ import { BASE_URL } from "./constant";
 export const connectSocket = (): Socket => {
   let socket;
   if(location.hostname === 'localhost'){
-   socket = io(BASE_URL, {
-      withCredentials: true,
-      autoConnect: true
-    });
+   socket = io(BASE_URL);
   }else{
     socket = io(BASE_URL, {
-      withCredentials: true,
+      // withCredentials: true,
       autoConnect: true,
       path: '/api/socket.io'
     });
